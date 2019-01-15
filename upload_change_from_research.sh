9ede726f71930e901aef0e85df475a23ac0d26c1
loveorchids <loveorchidsdavid@gmail.com.cn>
2019-01-15 12:56:50 +0900

    start

--- upload_change_from_research.sh
+++
@@ -1,8 +0,0 @@
-cp -r ~/Documents/omni_research/data      ~/Documents/omni_torch
-cp -r ~/Documents/omni_research/options   ~/Documents/omni_torch
-cp -r ~/Documents/omni_research/visualize ~/Documents/omni_torch
-cp ~/Documents/omni_research/networks/*.py ~/Documents/omni_torch/networks
-git config core.autocrlf true
-git add .
-git commit -m "start"
-git push
