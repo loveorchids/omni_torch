b1cadd23e908d2665ed530c8e5cf094f10cc667b
loveorchids <loveorchidsdavid@gmail.com>
2019-02-07 23:14:28 +0800

    Add comparison between Pytorch and Keras

--- examples/test/cifar-10_keras.py
+++ examples/test/cifar-10_keras.py
@@ -1,7 +1,10 @@
+try:
+    import keras
+except ImportError:
+    raise ImportError("You need to download keras>=2.2.0")
 from keras.models import Sequential
 from keras.layers import *
 from keras.datasets import *
-import keras
 from keras.preprocessing.image import ImageDataGenerator
 import os
 
@@ -22,7 +25,7 @@ x_test = x_test.astype('float')/255
 model = Sequential()
 model.add(Conv2D(32, 3, padding='same', input_shape=x_train.shape[1:], activation='relu'))
 model.add(Conv2D(32, 3, activation='relu'))
-model.add(MaxPooling2D())  # 默认pool_size=(2,2)
+model.add(MaxPooling2D())
 model.add(Dropout(0.25))
 
 model.add(Conv2D(64, 3, padding='same', activation='relu'))
@@ -37,7 +40,7 @@ model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
-model.summary()
+#model.summary()
 # exit(1)
 opt = keras.optimizers.adam(lr=1e-4, decay=1e-6)
 
@@ -51,16 +54,16 @@ if not data_augmentation:
 else:
     print('Using real-time data augmentation')
     datagen = ImageDataGenerator(
-        featurewise_center=False,  # 使输入数据集去中心化（均值为0）, 按feature执行
-        samplewise_center=False,  # 使输入数据的每个样本均值为0
-        featurewise_std_normalization=False,  # 将输入除以数据集的标准差以完成标准化, 按feature执行
-        samplewise_std_normalization=False,  # 将输入的每个样本除以其自身的标准差
-        zca_whitening=False,  # 对输入数据施加ZCA白化
-        rotation_range=0,  # 数据提升时图片随机转动的角度
-        width_shift_range=0.1,  # 图片宽度的某个比例，数据提升时图片水平偏移的幅度
-        height_shift_range=0.1,  # 图片高度的某个比例，数据提升时图片竖直偏移的幅度
-        horizontal_flip=True,  # 进行随机水平翻转
-        vertical_flip=False  # 进行随机竖直翻转
+        featurewise_center=False,
+        samplewise_center=False,
+        featurewise_std_normalization=False,
+        samplewise_std_normalization=False,
+        zca_whitening=False,
+        rotation_range=0,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True,
+        vertical_flip=False
     )
     datagen.fit(x_train)
     model.fit_generator(datagen.flow(x_train, y_train, batch_size=32), epochs=100, validation_data=(x_test, y_test), workers=4)  # 之前的keras必须要steps_per_epoch，workers：最大进程数
