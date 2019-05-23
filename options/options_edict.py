cc0e79a2eaec0455d6160882c83bafbe2c12487f
loveorchids <loveorchidsdavid@gmail.com>
2019-05-23 22:29:21 +0900

    default parameter change

--- options/options_edict.py
+++ options/options_edict.py
@@ -25,17 +25,19 @@ def initialize():
         "code_name": None,
         "cover_exist": True,
         "create_path":True,
+        "path": None,  # the directory contains the datasets
+        "extensions": ["jpeg", "JPG", "jpg", "png", "PNG", "gif", "tiff"],
 
-        "gpu_id": "0",
+        "gpu_id": None,
+        "output_gpu_id": 0,
         "epoch_num": 100,
         "deterministic_train": True,
         "seed": 1,
 
-        "batch_size": 8,
+        "batch_size_per_gpu": 8,
+        "cumulative_batch_size": None,
         "learning_rate":1e-4,
         "weight_decay":1e-4,
-        "batch_norm":True,
-        "finetune":False,
 
         "curr_epoch": 0,
         "epoches_per_phase": 1,
@@ -46,18 +48,19 @@ def initialize():
         "model3": None,
         "model4": None,
         "model5": None,
-
         
-        "loading_threads": 4, # how many cpu core to use to load data, usually 4 is sufficient
+        "loading_threads": 1, # how many cpu core to use to load data, usually 4 is sufficient
         "random_order_load": False,
-        "path": None, # the directory contains the datasets
-        "extensions": ["jpeg", "JPG", "jpg", "png", "PNG", "gif", "tiff"],
-
+        
         "img_channel": 3,
+        "img_mean": (0.5, 0.5, 0.5),
+        "img_std": (1.0, 1.0, 1.0),
+        "img_bias": (0.0, 0.0, 0.0),
+        "img_bit": 8,
+        
         "do_imgaug": False,
         "imgaug_order": "default",
         # default or a list, ["affine", "crop", "pad", ...], each element represent a process
-
         "do_affine": False, # See Documentation of imgaug affine
         # numbers in translations means pixel
         "translation_x": (0.0, 0.0),
@@ -69,12 +72,10 @@ def initialize():
         # numbers in translations means degree
         "rotation": (-0.0, 0.0),
         "aug_bg_color": 255,
-        
         # Crop
         # searh for CropToFixedSize in imgaug documentation
         "do_crop_to_fix_size": False,
         "crop_size": (224, 224),
-        
         # Pad
         # searh for PadToFixedSize in imgaug documentation
         "do_pad_to_fix_size": False,
@@ -83,30 +84,21 @@ def initialize():
         # corresponding dimension, respectively
         # or choose uniform to randomly place the image
         "padding_position": 'uniform',
-
         "do_random_flip": False,
         # v represent for vertical, h represent for horizontal
         "v_flip_prob": 0.5,
         "h_flip_prob": 0.5,
-
         "do_random_brightness": False,
         "brightness_vibrator": (1.0, 1.0),
         "multiplier": (1.0, 1.0),
         "multiplier_per_channel": False,
         "linear_contrast": 1.0,
-
         "do_random_noise": False,
         "gaussian_sigma": (0, 0.1),
-
         "to_final_size": False,
         "final_size": (224, 224),
-
         "standardize_size": False,
         "standardize_gcd": 8,
-
-        "img_mean": (0.5, 0.5, 0.5),
-        "img_std": (1.0, 1.0, 1.0),
-        "img_bias": (0.0, 0.0, 0.0),
     })
 
 if __name__ == "__main__":
